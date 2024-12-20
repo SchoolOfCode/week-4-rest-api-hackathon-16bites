8218a328a87313a7464721c7664d44e1505c59da
BeardedGrinn <mitchell.t.grinnell@hotmail.co.uk>
2024-12-20 09:16:50 +0000

    Made new app.js file
    Created new app.js file and created helper
    functions and endpoint functions for by number, by
    name and by type.

--- index.js
+++ index.js
@@ -1,66 +1,66 @@
-import express from 'express';
-import fs from "node:fs/promises";
-import { type } from 'node:os';
-const filename = 'pokemongen1.json';
+// import express from 'express';
+// import fs from "node:fs/promises";
+// import { type } from 'node:os';
+// const filename = 'pokemongen1.json';
 
-const app = express();
-const port = 3000
+// const app = express();
+// const port = 3000;
 
-// app.get("/", function(req, res) {
-//     res.send("Hello World!")
-// })
+// // app.get("/", function(req, res) {
+// //     res.send("Hello World!")
+// // })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
-
-
-
-//GET request - all Pokémon
-//make a get request to path "/" using async function
-app.get("/", async function(req,res) {
-    try {
-    const data = await fs.readFile(filename, "utf8");
-    const parsedData = JSON.parse(data);
-    res.status(200).json (
-    {
-        success: true,
-        payload: parsedData
-    });
-    } catch(error) {
-    res.status(500).json (
-    {
-        success: false,
-        payload: "Error"
-    });
-    }
-}
-);
+// app.listen(port, () => {
+//     console.log(`Example app listening on port ${port}`)
+//   })
 
 
-//Create  variable to store all of our Pokemon
-//return from an async function to resolve promise
-async function getAllPokemon() {
-    const data = await fs.readFile(filename, "utf8");
-    //Forgot to parse from JSON!
-    const allPokemons = JSON.parse(data);
-    // console.log(allPokemons);
-    return allPokemons;    
-}
 
-//get Pokemon by Number helper function
-//take in a given Pokemons number 1,2,3
-async function getPokemonByNumber(pokemonNumber) {
-    const allPokemons = await getAllPokemon();
-    // console.log(allPokemons);
+// //GET request - all Pokémon
+// //make a get request to path "/" using async function
+// app.get("/", async function(req,res) {
+//     try {
+//     const data = await fs.readFile(filename, "utf8");
+//     const parsedData = JSON.parse(data);
+//     res.status(200).json (
+//     {
+//         success: true,
+//         payload: parsedData
+//     });
+//     } catch(error) {
+//     res.status(500).json (
+//     {
+//         success: false,
+//         payload: "Error"
+//     });
+//     }
+// }
+// );
 
-    //filter allPokemons by "Number" = pokemonNumber
-    const pokemon = allPokemons.filter(function(pokemon) {
-        return pokemon.Number === pokemonNumber;
-        });
-    console.log(pokemon)
-    //store the resulting object in a new variable
-    //return the resulting object
-    }
 
-getPokemonByNumber(10)
+// //Create  variable to store all of our Pokemon
+// //return from an async function to resolve promise
+// async function getAllPokemon() {
+//     const data = await fs.readFile(filename, "utf8");
+//     //Forgot to parse from JSON!
+//     const allPokemons = JSON.parse(data);
+//     // console.log(allPokemons);
+//     return allPokemons;    
+// }
+
+// //get Pokemon by Number helper function
+// //take in a given Pokemons number 1,2,3
+// async function getPokemonByNumber(pokemonNumber) {
+//     const allPokemons = await getAllPokemon();
+//     // console.log(allPokemons);
+
+//     //filter allPokemons by "Number" = pokemonNumber
+//     const pokemon = allPokemons.filter(function(pokemon) {
+//         return pokemon.Number === pokemonNumber;
+//         });
+//     console.log(pokemon)
+//     //store the resulting object in a new variable
+//     //return the resulting object
+//     }
+
+// getPokemonByNumber(10)
